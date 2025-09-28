66bfc049063fabafb48bc4540c287f53b41bd5c9
Patan Sajid Ali Khan <sajid@ubuntu.com>
2025-09-28 12:12:17 +0530

    added column constraints with csv bind by name api

--- Backend/src/main/java/dev/sajid/backend/models/raw/Employee.java
+++ Backend/src/main/java/dev/sajid/backend/models/raw/Employee.java
@@ -1,14 +1,25 @@
 package dev.sajid.backend.models.raw;
 
+import com.opencsv.bean.CsvBindByName;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class Employee {
+    @CsvBindByName(column = "empid", required = true)
     private int empId;
+
+    @CsvBindByName(column = "pwd", required = true)
     private String pwd;
+
+    @CsvBindByName(column = "gender", required = true)
     private String gender;
+    
+    @CsvBindByName(column = "salu", required = false)
     private String salutation;
+
+    @CsvBindByName(column = "name", required = false)
     private String name;
 }
--- Backend/src/main/java/dev/sajid/backend/models/raw/Student.java
+++ Backend/src/main/java/dev/sajid/backend/models/raw/Student.java
@@ -1,16 +1,31 @@
 package dev.sajid.backend.models.raw;
 
+import com.opencsv.bean.CsvBindByName;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class Student {
+    @CsvBindByName(column = "roll", required = true)
     private String roll;
+
+    @CsvBindByName(column = "name", required = true)
     private String name;
+
+    @CsvBindByName(column = "branch", required = true)
     private String branch;
+
+    @CsvBindByName(column = "sec", required = true)
     private String section;
+
+    @CsvBindByName(column = "sem", required = true)
     private int sem;
+
+    @CsvBindByName(column = "scheme", required = true)
     private String scheme;
+
+    @CsvBindByName(column = "degr", required = true)
     private String degree;
 }
