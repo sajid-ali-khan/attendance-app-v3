5302cd3c2199cc77a0abd2d88dd41f7e6accd173
sajid <sajid@ubuntu.com>
2025-11-19 02:24:15 +0530

    modify security chain to allow ping to all

--- Backend/src/main/java/dev/sajid/backend/controllers/HealthController.java
+++ Backend/src/main/java/dev/sajid/backend/controllers/HealthController.java
@@ -1,12 +1,15 @@
 package dev.sajid.backend.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HealthController {
     @GetMapping("/ping")
-    public String ping(){
-        return "Ok";
+    public ResponseEntity<?> ping(){
+        return ResponseEntity.ok().build();
     }
 }
